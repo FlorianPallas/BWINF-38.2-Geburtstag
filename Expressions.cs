236ac0056158263ba917f15c3db2686ffff13212
Florian Pallas <pallas.florian@gmail.com>
2020-04-04 16:43:43 +0200

    initial commit

---
+++ Expressions.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace Geburtstag
+{
+    public static class Validation
+    {
+        private static double LimitLog = Math.Log(long.MaxValue);
+        private static double LimitLogLog = Math.Log(Math.Log(long.MaxValue));
+
+        public static bool ValidSum(long A, long B)
+        {
+            return long.MaxValue - A - B > 0;
+        }
+
+        public static bool ValidDifference(long A, long B)
+        {
+            return A > B;
+        }
+
+        public static bool ValidProduct(long A, long B)
+        {
+            return LimitLog >= Math.Log(A) + Math.Log(B);
+        }
+
+        public static bool ValidQuotient(long A, long B)
+        {
+            return A % B == 0;
+        }
+
+        public static bool ValidPower(long A, long B)
+        {
+            return LimitLogLog >= Math.Log(A) + Math.Log(Math.Log(B));
+        }
+
+        public static bool ValidFactorial(long A)
+        {
+            return A <= 20;
+        }
+    }
+
+    public abstract class Expression
+    {
+        protected string OperationString;
+        protected Expression A;
+        protected Expression B;
+        public long Value { get; protected set; }
+
+        public Expression(Expression A, Expression B)
+        {
+            this.A = A;
+            this.B = B;
+        }
+
+        public override string ToString()
+        {
+            return "(" + A.ToString() + OperationString + B.ToString() + ")";
+        }
+    }
+
+    public class ConstantExpression : Expression
+    {
+        public ConstantExpression(long _Value) : base(null, null)
+        {
+            Value = _Value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+
+    public class AddExpression : Expression
+    {
+        public AddExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "+";
+            Value = A.Value + B.Value;
+        }
+    }
+
+    public class SubtractExpression : Expression
+    {
+        public SubtractExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "-";
+            Value = A.Value - B.Value;
+        }
+    }
+
+    public class MultiplyExpression : Expression
+    {
+        public MultiplyExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "*";
+            Value = A.Value * B.Value;
+        }
+    }
+
+    public class DivideExpression : Expression
+    {
+        public DivideExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "/";
+            Value = A.Value / B.Value;
+        }
+    }
+
+    public class PowerExpression : Expression
+    {
+        public PowerExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "^";
+            Value = (long)Math.Pow(A.Value, B.Value);
+        }
+    }
+
+    public class ModuloExpression : Expression
+    {
+        public ModuloExpression(Expression A, Expression B) : base(A, B)
+        {
+            OperationString = "%";
+            Value = A.Value % B.Value;
+        }
+    }
+
+    public class FactorialExpression : Expression
+    {
+        public FactorialExpression(Expression A) : base(A, null)
+        {
+            OperationString = "!";
+            Value = A.Value;
+            for (long I = A.Value - 1; I >= 1; I--)
+            {
+                Value = Value * I;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "(" + A.ToString() + OperationString + ")";
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,216 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Geburtstag
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private enum EMode { TaskA, TaskB }
+
+        EMode Mode;
+        long TargetNumber;
+        int Digit;
+        bool SolutionFound;
+        Stopwatch Timer;
+        SortedDictionary<long, byte> Calculated;
+        List<List<Expression>> Expressions;
+
         public MainWindow()
         {
             InitializeComponent();
+            GroupBoxSolution.Visibility = Visibility.Collapsed;
+
+            Timer = new Stopwatch();
+            Calculated = new SortedDictionary<long, byte>();
+            Expressions = new List<List<Expression>>();
+        }
+
+        private void Calculate()
+        {
+            // Initialize
+            SolutionFound = false;
+            Calculated.Clear();
+            Expressions.Clear();
+            Timer.Restart();
+
+            // Edge cases
+            if (TargetNumber == 0)
+            {
+                Solution(new ConstantExpression(0), 0);
+            }
+
+            if (Digit == TargetNumber)
+            {
+                Solution(new ConstantExpression(TargetNumber), 1);
+            }
+
+            // Calculation
+            double Base = 0;
+            int I = 0;
+            while (!SolutionFound)
+            {
+                Expressions.Add(new List<Expression>());
+
+                // Calculate 'Base' | X, XX, XXX, XXXX
+                Base = Math.Pow(10, I) * Digit + Base;
+                Expressions[I].Add(new ConstantExpression((long)Base));
+
+                if (I == 0) { I++; continue; }
+
+                // Factorial
+                if (Mode == EMode.TaskB)
+                {
+                    AddFactorialExpressions(I - 1);
+                }
+
+                // Count up only to half the list (prevent further duplicates)
+                int Limit = (I + 1) / 2;
+                for (int J = 0; J < Limit; J++)
+                {
+                    int K = I - J - 1;
+                    foreach (Expression A in Expressions[J])
+                    {
+                        foreach (Expression B in Expressions[K])
+                        {
+                            CombineExpressions(I, A, B);
+                        }
+                    }
+                }
+
+                I++;
+            }
+        }
+
+        private void ParseInputs()
+        {
+            if (!long.TryParse(TextBoxNumber.Text.Trim(), out TargetNumber))
+            {
+                MessageBox.Show("Dieser Wert ist ungültig.", "Ungültige Eingabe", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (!int.TryParse(TextBoxDigit.Text.Trim(), out Digit) | Digit == 0)
+            {
+                MessageBox.Show("Diese Ziffer ist ungültig.", "Ungültige Eingabe", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+
+        public void AddFactorialExpressions(int Index)
+        {
+            // Factorial
+            for (int I = 0; I < Expressions[Index].Count; I++)
+            {
+                Expression E = Expressions[Index][I];
+                if (E.Value <= 2) { continue; }
+                if (Validation.ValidFactorial(E.Value))
+                {
+                    SaveExpression(Index, new FactorialExpression(E));
+                }
+            }
+        }
+
+        private void CombineExpressions(int Index, Expression A, Expression B)
+        {
+            // Addition
+            if (Validation.ValidSum(A.Value, B.Value))
+            {
+                SaveExpression(Index, new AddExpression(A, B));
+            }
+
+            // Subtraction
+            if (Validation.ValidDifference(A.Value, B.Value))
+            {
+                SaveExpression(Index, new SubtractExpression(A, B));
+            }
+            else
+            {
+                SaveExpression(Index, new SubtractExpression(B, A));
+            }
+
+            // Multiplication
+            if (Validation.ValidProduct(A.Value, B.Value))
+            {
+                SaveExpression(Index, new MultiplyExpression(A, B));
+            }
+            
+            // Division
+            if(Validation.ValidQuotient(A.Value, B.Value))
+            {
+                SaveExpression(Index, new DivideExpression(A, B));
+            }
+            if (Validation.ValidQuotient(B.Value, A.Value))
+            {
+                SaveExpression(Index, new DivideExpression(B, A));
+            }
+
+            // Power
+            if (Mode == EMode.TaskB && Validation.ValidPower(A.Value, B.Value))
+            {
+                SaveExpression(Index, new PowerExpression(A, B));
+            }
+            if (Mode == EMode.TaskB && Validation.ValidPower(B.Value, A.Value))
+            {
+                SaveExpression(Index, new PowerExpression(B, A));
+            }
+        }
+
+        private void SaveExpression(int Index, Expression A)
+        {
+            if (SolutionFound) { return; }
+            if (A.Value <= 0) { return; }
+
+            // Check for solution
+            if (A.Value == TargetNumber)
+            {
+                Solution(A, Index + 1);
+                return;
+            }
+
+            // Ignore duplicates
+            if (!Calculated.ContainsKey(A.Value))
+            {
+                Calculated.Add(A.Value, (byte)Index);
+                Expressions[Index].Add(A);
+            }
+        }
+
+        private void Solution(Expression Solution, int DigitCount)
+        {
+            Timer.Stop();
+            LabelSolution.Content = Solution.ToString() + " = " + Solution.Value;
+            LabelDigitCount.Content = DigitCount.ToString();
+            LabelTimeElapsed.Content = Timer.Elapsed.ToString(@"mm\:ss\.fff");
+            GroupBoxSolution.Visibility = Visibility.Visible;
+            SolutionFound = true;
+        }
+
+        private void Inputs_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (GroupBoxSolution == null) { return; }
+            GroupBoxSolution.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonBerechnenA_Click(object sender, RoutedEventArgs e)
+        {
+            Mode = EMode.TaskA;
+            ParseInputs();
+            Calculate();
+        }
+
+        private void ButtonBerechnenB_Click(object sender, RoutedEventArgs e)
+        {
+            Mode = EMode.TaskB;
+            ParseInputs();
+            Calculate();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
